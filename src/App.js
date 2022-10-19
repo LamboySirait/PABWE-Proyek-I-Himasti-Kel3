f7f36015535343509a5abdc7d97676535ca87948
ifs20028-IT Del <ifs20028@students.del.ac.id>
2022-10-19 21:28:01 +0700

    Divisi

--- src/App.js
+++ src/App.js
@@ -1,47 +1,37 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import NavigationBar from './components/NavigationBar';
+import NavigationBar from "./components/NavigationBar";
+import Divisi from "./pages/Divisi";
+import Home from "./pages/Home";
+import Bph from "./pages/Bph";
 
 // import Tentang from './pages/Tentang'
-// import Beranda from './pages/Home'
-import Home from './pages/Home';
-
 function App() {
   return (
     <div className="App">
-      <div >
-          <NavigationBar />
-      </div>
-
-      <div id='MainContent' style={{height: '1500px'}}>
-          <Home/>
-
-        {/* <Carousel>
-          <CarouselItem>
-            <div style={{height: '500px', backgroundColor: 'blue'}}>
-
-            </div> 
-          </CarouselItem> 
-          <CarouselItem>
-            <div style={{height: '500px', backgroundColor: 'yellow'}}></div>
-
-          </CarouselItem>
-
-        </Carousel> */}
-
-      </div>
-      
       <div>
+        <NavigationBar />
+      </div>
+      <div>
+        <Home />
+      </div>
 
-        <div className='bg-dark'>
-          <p className='text-white'>dawda</p>
+      <div>
+        <Bph />
+      </div>
+
+      <div>
+        <Divisi />
+      </div>
+      <div>
+        <div className="bg-dark">
+          <p className="text-white">dawda</p>
         </div>
 
-        <div className='container-fluid p-1 bg-white align-middle border-5' >
-            <p className='border align-middle'>Copyright</p>
+        <div className="container-fluid p-1 bg-white align-middle border-5">
+          <p className="border align-middle">Copyright</p>
         </div>
-
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,59 @@
-import { Carousel, CarouselItem } from 'react-bootstrap'
-import Tanoto from './../components/img/tanoto.jpg'
-import Logo from './../components/img/LogoHimasti.png'
+import { Carousel, CarouselItem } from "react-bootstrap";
+import Tanoto from "./../components/img/tanoto.jpg";
+import Logo from "./../components/img/LogoHimasti.png";
 
 const Home = () => {
-    return (
-        <div >
-            <div>
-                <Carousel className='justify-content-center m-auto' style={{height: '90vh'}}>
-                    <CarouselItem>
-                        <div style={{height: '90vh'}}>
-                            <img src={Tanoto} alt="" height='725px'/>
-                        </div> 
-                    </CarouselItem> 
-                    <CarouselItem>
-                        <div style={{height: '90vh', backgroundColor: 'gray'}}></div>
-                    </CarouselItem>
-                </Carousel>
+  return (
+    <div>
+      <div>
+        <Carousel
+          className="justify-content-center m-auto"
+          style={{ height: "90vh" }}
+        >
+          <CarouselItem>
+            <div style={{ height: "90vh" }}>
+              <img src={Tanoto} alt="" height="725px" />
             </div>
-            
-            <div className='mt-5 border justify-content-center m-auto' style={{width: '1300px'}}>
-                <h1 className='text-white'>Tentang Himasti</h1>
-                <div className='d-flex m-5 border'>
-                    <img className='m-lg-3' src={Logo} alt="" width='300px'/>
-                    <div className='text-white m-lg-3'>
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                </div>
-                <div className='d-flex m-5 text-white border'>
-                    <h1 className='m-lg-3'>Visi</h1>
-                    <p className='m-lg-3'>IT Del nama civitas kita, sungguh mulia cita citanya, tekadnya membangun manusia ilmunya dan wataknya</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+          </CarouselItem>
+          <CarouselItem>
+            <div style={{ height: "90vh", backgroundColor: "gray" }}></div>
+          </CarouselItem>
+        </Carousel>
+      </div>
 
-export default Home
+      <div
+        className="mt-5 border justify-content-center m-auto"
+        style={{ width: "1500px", height: "675px" }}
+      >
+        <h1 className="text-white">Tentang Himasti</h1>
+        <div className="d-flex m-5 border">
+          <img className="m-lg-3" src={Logo} alt="" width="300px" />
+          <div className="text-white m-lg-3">
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </div>
+        <div className="d-flex m-5 text-white border">
+          <h1 className="m-lg-3">Visi</h1>
+          <p className="m-lg-3">
+            IT Del nama civitas kita, sungguh mulia cita citanya, tekadnya
+            membangun manusia ilmunya dan wataknya
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
